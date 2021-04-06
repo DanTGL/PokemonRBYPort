6fd2a077ae6174836465efc522ce250d08f7b512
DanTGL <33489389+DanTGL@users.noreply.github.com>
2021-04-06 21:55:19 +0200

    Refactored code for data extractor

--- extract_data.py
+++ extract_data.py
@@ -1,4 +1,6 @@
+import os
 import json
+import struct
 
 FIRST_POKEMON_ENTRY = 0x0383DE
 
@@ -8,33 +10,42 @@ pokemon = {}
 
 # Extract pokemon data
 
-with open("pokemon.rom", "rb") as file:
+def read_bytes(file, size):
+    return int.from_bytes(file.read(size), byteorder="little")
+
+with open("pokemon.gb", "rb") as file:
     file.seek(FIRST_POKEMON_ENTRY)
 
     for i in range(NUM_POKEMON):
-        dex_num = file.read(1)
+        dex_num = struct.unpack("B", file.read(1))[0]
         pokemon[dex_num] = {}
-        pokemon[dex_num]["BaseHP"] = file.read(1)
-        pokemon[dex_num]["BaseAttack"] = file.read(1)
-        pokemon[dex_num]["BaseDefense"] = file.read(1)
-        pokemon[dex_num]["BaseSpeed"] = file.read(1)
-        pokemon[dex_num]["BaseSpecial"] = file.read(1)
-        pokemon[dex_num]["Type1"] = file.read(1)
-        pokemon[dex_num]["Type2"] = file.read(1)
-        pokemon[dex_num]["CatchRate"] = file.read(1)
-        pokemon[dex_num]["BaseExpYield"] = file.read(1)
+        pokemon[dex_num]["BaseHP"] = read_bytes(file, 1)
+        pokemon[dex_num]["BaseAttack"] = read_bytes(file, 1)
+        pokemon[dex_num]["BaseDefense"] = read_bytes(file, 1)
+        pokemon[dex_num]["BaseSpeed"] = read_bytes(file, 1)
+        pokemon[dex_num]["BaseSpecial"] = read_bytes(file, 1)
+        pokemon[dex_num]["Type1"] = read_bytes(file, 1)
+        pokemon[dex_num]["Type2"] = read_bytes(file, 1)
+        pokemon[dex_num]["CatchRate"] = read_bytes(file, 1)
+        pokemon[dex_num]["BaseExpYield"] = read_bytes(file, 1)
 
-        pokemon[dex_num]["SpriteDims"] = file.read(1)
-        pokemon[dex_num]["fSpritePtr"] = file.read(2)
-        pokemon[dex_num]["bSpritePtr"] = file.read(2)
+        pokemon[dex_num]["SpriteDims"] = read_bytes(file, 1)
+        pokemon[dex_num]["fSpritePtr"] = read_bytes(file, 2)
+        pokemon[dex_num]["bSpritePtr"] = read_bytes(file, 2)
 
         # Read all four moves
-        pokemon[dex_num]["Lvl1Attacks"] = [file.read(1) for i in range(4)]
-        pokemon[dex_num]["GrowthRate"] = file.read(1)
-        pokemon[dex_num]["MoveFlags"] = file.read(7)
+        pokemon[dex_num]["Lvl1Attacks"] = [read_bytes(file, 1) for i in range(4)]
+        pokemon[dex_num]["GrowthRate"] = read_bytes(file, 1)
+        pokemon[dex_num]["MoveFlags"] = read_bytes(file, 7)
         
         # Read and discard padding
         file.read(1)
+
+if not os.path.exists("data"):
+    os.mkdir("data")
+
+for key, value in pokemon.items():
+    print(key, value, "\n")
 
 # Output data to json file
 with open("data/pokemon.json", "w") as json_file:
