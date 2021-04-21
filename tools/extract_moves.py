d04fc29ec716f0b9fa975b30e41f6cec7e97ac5c
DanTGL <33489389+DanTGL@users.noreply.github.com>
2021-04-21 12:54:08 +0200

    Extraction of moves to json file

--- tools/extract_moves.py
+++ tools/extract_moves.py
@@ -1,8 +1,11 @@
 from text_utils import *
+import json
 
 FIRST_MOVE_NAME = "POUND"
 
 NUM_MOVES = 165
+
+FIRST_MOVE_ENTRY = b"\x01\x00\x28\x00\xFF\x23"
 
 def read_bytes(file, size):
     return int.from_bytes(file.read(size), byteorder="little")
@@ -13,7 +16,7 @@ def find_name_offset(rom):
 def extract_names(rom):
     rom.seek(find_name_offset(rom.read()))
 
-    moves = []
+    names = []
 
     for i in range(NUM_MOVES):
         characters = []
@@ -26,10 +29,38 @@ def extract_names(rom):
                 
             characters.append(text_decode([c]))
         
-        moves.append("".join(characters))
+        names.append("".join(characters))
     
-    print("\n".join(moves))
+    return names
+
+def extract_moves(rom, names):
+    moves = []
+
+    rom.seek(0)
+    rom.seek(rom.read().find(FIRST_MOVE_ENTRY))
+
+    for i in range(NUM_MOVES):
+        id = read_bytes(rom, 1)
+
+        moves.append(dict())
+        moves[i]["id"] = id
+        moves[i]["name"] = names[i]
+        moves[i]["effect"] = read_bytes(rom, 1)
+        moves[i]["power"] = read_bytes(rom, 1)
+        moves[i]["type"] = read_bytes(rom, 1)
+        moves[i]["accuracy"] = read_bytes(rom, 1)
+        moves[i]["pp"] = read_bytes(rom, 1)
+    
+    return moves
 
 if __name__ == "__main__":
     with open("pokemon.gb", "rb") as f:
-        extract_names(f)
+        moves = extract_moves(f, extract_names(f))
+
+        json_file = open("data/moves.json", "w")
+
+        json.dump(moves, json_file)
+
+        json_file.close()
+
+
